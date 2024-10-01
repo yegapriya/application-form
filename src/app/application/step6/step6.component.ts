0c1d260d8b9564ea840e68738521a1650ea59a76
ayesha0806 <ayeshar080604@email.com>
2024-10-01 11:26:26 +0530

    new form

--- src/app/application/step6/step6.component.ts
+++ src/app/application/step6/step6.component.ts
@@ -19,6 +19,7 @@ export class Step6Component implements OnInit {
   selectedDrop: SelectItem = { value: '' };
   cities: any[] = [];
   filteredCountries: any[] = [];
+ 
 
   // FormGroup Definition
   form: FormGroup = new FormGroup({
@@ -70,6 +71,30 @@ export class Step6Component implements OnInit {
     this.filteredCountries = filtered;
   }
   goToStep5(){
+    
     this.router.navigate(['/form/step5']);
   }
+  onSubmit() {
+    if (this.form.valid) {
+      console.log("Diploma Branch Studied:", this.form.get('value1')?.value);
+      console.log("Name of the Board:", this.form.get('value2')?.value);
+      console.log("% of Marks:", this.form.get('value3')?.value);
+      console.log("Sem-1:", this.form.get('value4')?.value);
+      console.log("Sem-2:", this.form.get('value5')?.value);
+      console.log("Sem-3:", this.form.get('value6')?.value);
+      console.log("Sem-4:", this.form.get('value7')?.value);
+      console.log("Sem-5:", this.form.get('value8')?.value);
+      console.log("Sem-6:", this.form.get('value9')?.value);
+      console.log("Sem-7:", this.form.get('value10')?.value);
+      console.log("Sem-8:", this.form.get('value11')?.value);
+      console.log("Name of the Bank:", this.form.get('value12')?.value);
+      console.log("Name of the Branch/Place:", this.form.get('value13')?.value);
+      console.log("Bank Account Number:", this.form.get('value14')?.value);
+      console.log("MICR Code of the Bank:", this.form.get('value15')?.value);
+      console.log("IFSC Code of the Bank:", this.form.get('value16')?.value);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
+  
 }
